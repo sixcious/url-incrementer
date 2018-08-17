335b726b85b5d16903b64ee6cd40f7c7115b0c1d
roysix <royxsix@gmail.com>
2018-08-17 13:43:13 -0400

    .

--- src/chrome/js/cryptography.js
+++ src/chrome/js/cryptography.js
@@ -10,16 +10,16 @@ var URLI = URLI || {};
 URLI.Cryptography = function () {
 
   /**
-   * Calculates a cryptographic hash. We use the PBKDF2WithHmacSHA512 algorithm and hash.
-   * For simplicity, we hardcode the algorithm, hash, and iterations.
+   * Calculates a cryptographic hash. We use the PBKDF2 algorithm with an Hmac-SHA512 hash function.
+   * For simplicity, we hardcode the algorithm, hash, and iterations. Note: 512 Bits = 64 Bytes = 88 B64 Characters.
    *
-   * @param plaintext
-   * @param salt
+   * @param text the text to hash
+   * @param salt the salt to hash with
    * @returns {Promise<string>} the hash
    * @public
    */
-  async function calculateHash(plaintext, salt) {
-    const key = await crypto.subtle.importKey("raw", new TextEncoder().encode(plaintext ? plaintext : "gUmMiBeARs"), "PBKDF2", false, ["deriveBits"]); // Firefox: Hangs if the plaintext is empty
+  async function calculateHash(text, salt) {
+    const key = await crypto.subtle.importKey("raw", new TextEncoder().encode(text ? text : "gUmMiBeARs"), "PBKDF2", false, ["deriveBits"]); // Firefox: Hangs if the text is empty
     const bits = await crypto.subtle.deriveBits({name: "PBKDF2", hash: "SHA-512", salt: str2u8a(salt), iterations: 1337}, key, 512);
     return u8a2str(new Uint8Array(bits));
   }
@@ -42,7 +42,6 @@ URLI.Cryptography = function () {
    * @private
    */
   function u8a2str(u8a) {
-    //return btoa(String.fromCharCode.apply(null, u8a));
     return btoa(String.fromCharCode(...u8a));
   }
 
@@ -54,7 +53,7 @@ URLI.Cryptography = function () {
    * @private
    */
   function str2u8a(str) {
-    return new Uint8Array(atob(str).split("").map(function(c) { return c.charCodeAt(0); }));
+    return new Uint8Array([...atob(str)].map(c => c.charCodeAt(0)));
   }
 
   // Return Public Functions
