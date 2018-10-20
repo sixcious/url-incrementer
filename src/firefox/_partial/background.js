831d74bd74f1370c68ba5543847fd993fac1ccad
Roy Six <roysix@users.noreply.github.com>
2018-10-20 12:36:52 -0400

    .

--- src/firefox/_partial/background.js
+++ src/firefox/_partial/background.js
@@ -1,6 +1,6 @@
 // Replace storage default permissions (const STORAGE_DEFAULT_VALUES)
     "permissionsInternalShortcuts": true, "permissionsDownload": false, "permissionsEnhancedMode": true,
 
-// Replace URL Increment and URL Decrement extensions IDs
-const URL_INCREMENT_EXTENSION_ID = "decebmdlceenceecblpfjanoocfcmjai@url-increment",
-      URL_DECREMENT_EXTENSION_ID = "nnmjbfglinmjnieblelacmlobabcenfk@url-decrement";
+// Replace URL Increment and URL Decrement extensions IDs (messageExternalListener)
+    const URL_INCREMENT_EXTENSION_ID = "url-increment@webextensions",
+          URL_DECREMENT_EXTENSION_ID = "url-decrement@webextensions";
